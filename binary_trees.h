eed7fe9319c964f551fccf9f7983465eea668742
rayen445 <nesstinebacc@gmail.com>
2024-04-10 15:49:42 +0200

    commit

---
+++ binary_trees.h
@@ -0,0 +1,16 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/* Basic binary tree structure */
+typedef struct binary_tree_s {
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
+
+/* Function prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+
+#endif /* BINARY_TREES_H */
