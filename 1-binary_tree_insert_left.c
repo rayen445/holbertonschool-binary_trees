5a7237d509a7a2114691595ec7c16f5062c50bd7
rayen445 <nesstinebacc@gmail.com>
2024-04-10 15:57:31 +0200

    commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,31 +10,31 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    /* Allocate memory for new node */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocate memory for new node */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Initialize new node */
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	/* Initialize new node */
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    /* If parent already has a left child, replace it */
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        new_node->left->parent = new_node;
-    }
+	/* If parent already has a left child, replace it */
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
 
-    /* Set the new node as the left child of parent */
-    parent->left = new_node;
+	/* Set the new node as the left child of parent */
+	parent->left = new_node;
 
-    return (new_node);
+	return (new_node);
 }
