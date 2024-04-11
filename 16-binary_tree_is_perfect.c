2bd6cd21be9721e0a7e252a7d12140205a143785
rayen445 <nesstinebacc@gmail.com>
2024-04-11 13:31:13 +0200

    commit

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure the height of
+ *
+ * Return: The height of the tree, 0 if @tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	return (right_height + 1);
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: A pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height, expected_nodes, actual_nodes;
+
+	if (tree == NULL)
+		return (0);
+
+	height = binary_tree_height(tree);
+	expected_nodes = (1 << height) - 1;
+	actual_nodes = binary_tree_size(tree);
+
+	return (actual_nodes == expected_nodes);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -3,16 +3,22 @@
 
 #include <stddef.h>
 
-/* Basic Binary Tree Structure */
+/* Basic binary tree structure */
 typedef struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 } binary_tree_t;
 
-/* Function Prototypes */
+/* Function prototypes */
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
