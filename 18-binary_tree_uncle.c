5703178607e30e355c6d0cf53267fae459e91d3d
rayen445 <nesstinebacc@gmail.com>
2024-04-11 13:38:07 +0200

    commit

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -4,15 +4,15 @@
  * binary_tree_uncle - Finds the uncle of a node
  * @node: A pointer to the node to find the uncle
  *
- * Return: A pointer to the uncle node, or NULL if node is NULL or has no uncle
+ * Return: A pointer to the uncle node, or NULL if no uncle is found
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
 
-    if (node->parent->parent->left == node->parent)
-        return (node->parent->parent->right);
-    else
-        return (node->parent->parent->left);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
 }
