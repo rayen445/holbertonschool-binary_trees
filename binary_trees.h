b6301dbce7d4f8b6dc7fc46836cab496937cc16c
rayen445 <nesstinebacc@gmail.com>
2024-04-11 12:39:44 +0200

    commit

--- binary_trees.h
+++ binary_trees.h
@@ -19,5 +19,6 @@ void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
 int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif /* BINARY_TREES_H */
