8c08e78dcc68e47cc06389e45bc5cca324cc998c
rayen445 <nesstinebacc@gmail.com>
2024-04-11 12:53:06 +0200

    commit

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -14,7 +14,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_postorder(tree->left, func); /* Recur on left subtree */
-	binary_tree_postorder(tree->right, func); /* Recur on right subtree */
-	func(tree->n); /* Execute function on current node */
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
