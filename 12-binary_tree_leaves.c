d5b341f5be191dec0ed32aa3ca1ab44d05c20a64
rayen445 <nesstinebacc@gmail.com>
2024-04-11 13:14:08 +0200

    commit

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,17 +2,17 @@
 
 /**
  * binary_tree_leaves - Counts the leaves in a binary tree
- * @tree: Pointer to the root node of the tree to count the leaves
+ * @tree: Pointer to the root node of the tree to count the number of leaves
  *
- * Return: The number of leaves in the binary tree, or 0 if tree is NULL
+ * Return: Number of leaves in the binary tree, or 0 if tree is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
