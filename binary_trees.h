b6c2d9a6e17f93307ac0811befc04adb076662d9
rayen445 <nesstinebacc@gmail.com>
2024-04-11 13:37:06 +0200

    commit

--- binary_trees.h
+++ binary_trees.h
@@ -4,14 +4,18 @@
 #include <stddef.h>
 
 /* Basic Binary Tree Structure */
-typedef struct binary_tree_s {
+typedef struct binary_tree_s binary_tree_t;
+
+struct binary_tree_s
+{
     int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-} binary_tree_t;
+    binary_tree_t *parent;
+    binary_tree_t *left;
+    binary_tree_t *right;
+};
 
 /* Function Prototypes */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif /* BINARY_TREES_H */
